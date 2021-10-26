38d7731d4f38e23095864b75682c99768b777760
Ted M. Young <tedyoung@gmail.com>
2021-10-26 15:22:44 -0700

    Lab #8: Player Hits, does NOT go Bust, and GameMonitor.roundCompleted should NOT be called.

--- src/test/java/com/jitterted/ebp/blackjack/domain/port/GameMonitorTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/port/GameMonitorTest.java
@@ -5,6 +5,7 @@ import com.jitterted.ebp.blackjack.domain.StubDeck;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
@@ -37,4 +38,16 @@ class GameMonitorTest {
         verify(gameMonitorSpy).roundCompleted(any(Game.class));
     }
 
+    @Test
+    public void playerHitsDoesNotGoBustThenMonitorIsNotCalled() throws Exception {
+        GameMonitor gameMonitorSpy = spy(GameMonitor.class);
+        Game game = new Game(StubDeck.playerHitsAndDoesNotGoBust(),
+                             gameMonitorSpy);
+        game.initialDeal();
+
+        game.playerHits();
+
+        verify(gameMonitorSpy, never()).roundCompleted(any(Game.class));
+    }
+    
 }
